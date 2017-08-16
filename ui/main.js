3498c74fee640581c1c31098d6b653ed25778102
akshatha-s13 <30971444+akshatha-s13@users.noreply.github.com>
2017-08-16 20:58:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,7 @@ var element=document.getElementById('main-text')
 element
 
 element.innerHTML='akkkkshhu';
+var img=document.getElementById('madi')
+img.onclick=function(){
+    img.style.marginLeft='100px';
+}
