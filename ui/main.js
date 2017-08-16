8dc6a4390dc97179cdebed4d1345e1a0476a7346
akshatha-s13 <30971444+akshatha-s13@users.noreply.github.com>
2017-08-16 21:03:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element=document.getElementById('main-text')
 element
 
 element.innerHTML='akkkkshhu';
-var img=document.getElementById('madi')
+var img=document.getElementById('madi');
+marginLeft=0;
+function moveRight(){
+    marginLeft+=10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+   // img.style.marginLeft='100px';
 }
