83dadc6aa2b1f2b320fbfb82435804642b7cdd9b
akshatha-s13 <30971444+akshatha-s13@users.noreply.github.com>
2017-08-19 01:00:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,10 +45,11 @@ submit.onclick=function(){
     ul.innerHTML = list;
     }
     }
+    }
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
     request.open('GET','http://akshathas513.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
-}
+
   
 };
