cc7387011d8900bd56b7770cbbf1d2902e67b88a
akshatha-s13 <30971444+akshatha-s13@users.noreply.github.com>
2017-08-22 18:04:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,55 +1,24 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element
-element.innerHTML='akkkkshhu';
-var img=document.getElementById('madi');
-marginLeft=0;
-function moveRight(){
-    marginLeft+=10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-   // img.style.marginLeft='100px';
-}
 
-var button=document.getElementById('counter');
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-    if(request.readyState==XMLHttpRequest.DONE){
-        if(request.status==200){
-        var ctr=request.responseText;
-      var span=document.getElementById('count');
-span.innerHTML=ctr.toString();  
-    }
-    }
-}
-request.open('GET','http://akshathas513.imad.hasura-app.io/counter',true);
-request.send(null);
-}
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
  var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
     if(request.readyState==XMLHttpRequest.DONE){
-        if(request.status==200){
-         var names=request.responseText;
-         names=JSON.parse(names);
-   var list='';
-   for(var i=0;i<names.length;i++)
-   {
-   list+='<li>'+names[i]+'</li>';
+        if(request.status==200)
+        alert ('Login successful');
+        else if(request.status==500)
+        alert('Something went wrong');
+        else if(request.status==403)
+        alert('Invalid credentials');
+    
     }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
-    }
-    }
-    }
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-    request.open('GET','http://akshathas513.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
+    
+    
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST','http://akshathas513.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password : password}));
 
-  
 };
+
